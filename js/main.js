b654c4827d29a03051e94ba579a8889cfe447ec4
filip-karlsson-nti-johanneberg <filip.karlsson@elev.ga.ntig.se>
2021-03-21 02:23:53 +0100

    lite fixer upper dåva

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
-function toggleMeny() {
-    document.querySelector('.')
+const nav = document.querySelector('.ListHeader')
+const burgare = document.querySelector('.burgare')
+
+function toggleMenu() {
+    nav.classList.toggle('activemenu')
+    burgare.classList.toggle('toggle')
+    console.log('hej')
+
 }
+
+burgare.addEventListener('click', toggleMenu)
